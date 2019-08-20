fd8ffa3d22d1d9fa85d77f2092cc562b7b42ebdc
Francis Bourgouin <netbeuix@gmail.com>
2019-08-20 13:11:11 -0400

    Adding comments & cleanup of demo code

--- README.md
+++ README.md
@@ -2,18 +2,53 @@
 
 ## Review of the curriculum
 
+Slides ! : https://docs.google.com/presentation/d/1m3R_aN4S5YoCBmXRbjaZQGatygWyZXYLcN-fkcP_HWA/edit?usp=sharing
+
 ## How to approach a problem
+
+- Properly define our problem
+- Make smaller goals out of the original one
+- Don't stress if your original solution is not the best code written
+- Refactor once your code is working, and make sure it keeps working
 
 ## Live coding !
 
+We want to build a script that reads arguments in the terminal and gives us the sum.
+
+- How many numbers ? (0 - big number (not infinite))
+- How to capture arguments ? process.argv
+- Return or print ? Functions should return a result, then we output that using console.log
+- What language ? JavaScript ! :D
+- Node vs JS ? Node will interpret the JavaScript code.
+
 ### What you should while I code
+
+In a live demo, it's always better to look & understand the code being written and to participate in it's creation since it's more important to understand the why and how more than replicate without understanding what's happening.
 
 ### Demo with process.argv
 
+Let's make our script, and refactor once we have a functionning script ! See file demo.js
+
 ### Why are you coding this ?
+
+When you write code, it's important to understand why we are writing it. So don't hesitate to write your pseudo-code in comments then fill your pseudo code with working code. Also, sometimes it's tempting to use code found on the internet, but if you cannot explain simply in english what the method or function does, try to find another way or take the time to understand said method or function.
 
 ### Uh oh, errors :/
 
+Errors are a part of a coder's life. They are there to help so take the time to read what the error is telling you, and understand what caused it to find the solution.
+
 ### How to search aka. how to word a problem
 
+Once you encounter a problem, don't panic, it's normal, then explain to yourself the problem and use that explanation to formulate a good search to help you find a solution.
+
 ## Git workflow
+
+Don't forget that your github account is your coder's portfolio, so it's always a good idea to commit often and write meaningful commit messages. A classic workflow from the creation of your project folder to publishing your changes is :
+
+- git init (in your project folder)
+- git remote add origin *URLOFYOURGITHUBREPOSITORY*
+- git add . (to add everything, or name of the file instead of the .)
+- git commit -m "MEANINGFUL MESSAGE"
+- git push origin master
+
+The last two steps should be done frequently, every time you completed a small goal in your project. 
--- demo.js
+++ demo.js
@@ -1,31 +1,45 @@
 /**
  * Read arguments from the command line
  * use process.argv
- * 
+ *
  * Create sum of all the arguments
  * arguments are all strings !?c
  * first two are node path & file path
- * 
+ *
  * Validate the result and output
  * ??
- * 
+ *
  */
 
+// Test what process.argv returns
 console.log(process.argv)
 // Results in an array containing [pathofnode, pathoffile, then arguments....]
-const convertToNumberSkippingNaN = function(value) {
-  const convertedValue = Number(value)
-  return Number.isNaN(convertedValue) ? 0 : convertedValue
+
+//First write of our code
+
+let output = 0;
+
+for (let iterator = 2; iterator < process.argv.length; iterator++) {
+  // Changed the iterator starter to 2 since we don't need the first 2 arguments
+  // We need to change the values to integers and not text (string)
+  
+  const currentArgument = Number(process.argv[iterator])
+  //If currentArgument is not NaN, output it, no need for a else.
+  
+  if (!Number.isNaN(currentArgument)) {
+    output += currentArgument;
+  }
+  // We can use ternary operators ( question ? true : false ) instead
+  // output += Number.isNaN(currentArgument) ? 0 : currentArgument;
 }
 
-console.log(convertToNumberSkippingNaN('10'))
-console.log(convertToNumberSkippingNaN(10))
-console.log(convertToNumberSkippingNaN('Bob'))
+console.log(output)
 
+// First iteration of our refactoring, using for..of and slice
 
 // Prepare the array with only our values
 const cleanedArray = process.argv.slice(2)
-// we could use a for.. of loop
+// We could use a for.. of loop
 let newOutput = 0
 for (const value of cleanedArray) {
   const convertedValue = Number(value)
@@ -33,23 +47,39 @@ for (const value of cleanedArray) {
 }
 console.log(newOutput)
 
+// Second iteration of refactoring, using functions to modularize our code
 
-// If command line input is "node demo.js 1 3 4 6 7"
-let output = 0;
-
-for (let iterator = 2; iterator < process.argv.length; iterator++) {
-  //Changed the iterator starter to 2 since we don't need the first 2 arguments
-  
-  //We need to change the values to integers and not text (string)
-
-  const currentArgument = Number(process.argv[iterator])
-
-  if (!Number.isNaN(currentArgument)) {
-    output += currentArgument;
-  }
-
-  //We can use ternary operators ( question ? true : false ) instead
-  // output += Number.isNaN(currentArgument) ? 0 : currentArgument;
+const convertToNumberSkippingNaN = function(value) {
+  const convertedValue = Number(value)
+  return Number.isNaN(convertedValue) ? 0 : convertedValue
 }
 
-console.log(output)
+// Testing if the function works properly
+
+console.log(convertToNumberSkippingNaN('10'))
+console.log(convertToNumberSkippingNaN(10))
+console.log(convertToNumberSkippingNaN('Bob'))
+
+const prepareArray = function(array) {
+  const output = array.slice(2)
+  const cleanedOutput = []
+  for (const element of output) {
+    cleanedOutput.push(convertToNumberSkippingNaN(element))
+  }
+  return cleanedOutput
+}
+
+// Testing if prepareArray give us an Array of numbers
+console.log(prepareArray(['1','bob','chicken',5,'10','-10']))
+
+let sum = 0
+for (const number of prepareArray(process.argv)) {
+  sum += number
+}
+console.log(sum)
+
+let substract = 0
+for (const number of prepareArray(process.argv)) {
+  substract -= number
+}
+console.log(substract)
